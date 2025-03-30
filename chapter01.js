accada27ce410fa681874597c597b639dae43fce
tmdgusya <dev0jsh@gmail.com>
2025-03-30 16:04:13 +0900

    init

---
+++ chapter01.js
@@ -0,0 +1,12 @@
+var myName = "Kyle";
+var age;
+var adult = true;
+
+if (adult) {
+    var myName = "Kyle";
+    let age = 39;
+    console.log("Secret!");
+}
+
+console.log(myName); // Kyle
+console.log(age); // undefined
